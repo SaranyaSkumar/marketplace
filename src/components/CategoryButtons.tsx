2dc4fd137daaf38c5d8a230665a2c7c9f4b79189
Saranya <saranya.k@loomy.com>
2024-01-13 16:22:20 +1300

    images

--- src/components/CategoryButtons.tsx
+++ src/components/CategoryButtons.tsx
@@ -10,7 +10,7 @@ const CategoryButtons = ({ currentCategory }:any) => {
   return (
     <div className="flex flex-col space-y-2 mt-8">
       {categories.map((category) => (
-        <Link key={category.name} href={category.path} className={`px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:bg-indigo-700 ${currentCategory === category.name.toLowerCase() ? 'bg-indigo-700' : 'text-black'}`}>
+        <Link key={category.name} href={category.path} className={`px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white hover:bg-indigo-700 ${currentCategory === category.name.toLowerCase() ? 'bg-indigo-500' : 'text-black'}`}>
             {category.name}
         </Link>
       ))}
